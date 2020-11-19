a4bd9d234fc7219688e240e271a679f63cff63bf
william-cantwell <72945395+william-cantwell@users.noreply.github.com>
2020-11-19 09:42:35 -0500

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+__author__ = "William Cantwell"
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
---
+++ blog/views.py
@@ -0,0 +1,16 @@
+
+__author__ = "William Cantwell"
+
+from django.views.generic import ListView, DetailView # new
+
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = 'post_detail.html'
